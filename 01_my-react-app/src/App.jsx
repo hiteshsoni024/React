d71e590eca7b42c40c5afa371178e7e3e6129247
hiteshsoni024 <sonihitesh2024@gmail.com>
2025-01-30 13:27:35 +0530

    First React file

---
+++ 01_my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import Games from "./Games.jsx"
+function App() {
+  return(
+    <>
+    <Header/>
+    <Games/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ 01_my-react-app/src/Games.jsx
@@ -0,0 +1,15 @@
+function Games(){
+    const HeavyGame1 = "Call of Duty";
+    const HeavyGame2 = "Battleground mobile India";
+    return(
+        <ol>
+            <li>Angry bird</li>
+            <li>Candy Crush</li>
+            <li>Subway Suffer</li>
+            <li>Granny</li>
+            <li>{HeavyGame1}</li>
+            <li>{HeavyGame2.toUpperCase()}</li>
+        </ol>
+    );
+}
+export default Games
